d2d9b08a4d82134d9f8dc9d34a83262610ff4a68
Joseph Afonne <josef.afonne@gmail.com>
2026-01-30 19:51:50 +0200

    add css to admin

--- backend/backend_advice_portal/settings.py
+++ backend/backend_advice_portal/settings.py
@@ -178,7 +178,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+# Where collectstatic will put everything (created automatically)
+STATIC_ROOT = BASE_DIR / 'static'
 
 # media files
 MEDIA_URL = '/media/'
